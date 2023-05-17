16d4756c3556b17e22f7eda17f7085d2a8391146
Yukihiro1123 <murasima1123@gmail.com>
2023-05-17 20:34:21 +0900

    stack_trace削除

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_training/app.dart';
 import 'package:riverpod_training/firebase_options.dart';
-import 'package:stack_trace/stack_trace.dart' as stack_trace;
+//import 'package:stack_trace/stack_trace.dart' as stack_trace;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,9 +11,9 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const ProviderScope(child: MyApp()));
-  FlutterError.demangleStackTrace = (StackTrace stack) {
-    if (stack is stack_trace.Trace) return stack.vmTrace;
-    if (stack is stack_trace.Chain) return stack.toTrace().vmTrace;
-    return stack;
-  };
+  // FlutterError.demangleStackTrace = (StackTrace stack) {
+  //   if (stack is stack_trace.Trace) return stack.vmTrace;
+  //   if (stack is stack_trace.Chain) return stack.toTrace().vmTrace;
+  //   return stack;
+  // };
 }
