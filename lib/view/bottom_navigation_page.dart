f53fb389d2e2691976f2cf0d402f690ef11a0f2d
吉田光輝 <yoshidamitsuteru@yoshidamitsuterus-MacBook-Pro.local>
2023-05-21 22:26:36 +0900

    bottomNaviをhooksで

--- lib/view/bottom_navigation_page.dart
+++ lib/view/bottom_navigation_page.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_training/config/utils/enum/router_enum.dart';
 import 'package:riverpod_training/provider/bottom_navi/bottom_navi_provider.dart';
+import 'package:riverpod_training/routing/app_router.dart';
 
-class BottomNavigationPage extends ConsumerWidget {
+class BottomNavigationPage extends HookConsumerWidget {
   const BottomNavigationPage({super.key, required this.child});
   final Widget child;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final selectedIndex = useState(0);
+    useEffect(() {
+      //現在のパス取得
+      final String currentPath = ref.read(goRouterProvider).location;
+      if (currentPath == AppRoute.tasks.path) {
+        selectedIndex.value = 0;
+      } else if (currentPath == AppRoute.users.path) {
+        selectedIndex.value = 1;
+      } else if (currentPath == AppRoute.mypage.path) {
+        selectedIndex.value = 2;
+      } else {
+        selectedIndex.value = 0;
+      }
+      return null;
+    }, [
+      /// ここに変更を監視したい変数を入れる
+      ref.watch(goRouterProvider).location,
+    ]);
     return Scaffold(
       body: child,
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: ref.watch(bottomNaviSelectedIndexProvider),
+        currentIndex: selectedIndex.value,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -28,7 +48,8 @@ class BottomNavigationPage extends ConsumerWidget {
           ),
         ],
         onTap: (int value) {
-          ref.read(bottomNaviSelectedIndexProvider.notifier).change(value);
+          selectedIndex.value = value;
+          // ref.read(bottomNaviSelectedIndexProvider.notifier).change(value);
           switch (value) {
             case 0:
               context.goNamed(AppRoute.tasks.name);
